18b2922a7567f6a9bfc5db55d8a99e322e11f673
Farid Virani <fvirani786@gmail.com>
2024-04-02 21:31:22 -0500

    feat:header elemente

--- js/app.js
+++ js/app.js
@@ -6,3 +6,13 @@
 
 /*==================== scroll reveal ====================*/
 /*==================== typed js ====================*/
+
+const header = document.createElement('header');
+header.innerHTML = '<h1>This is the Header</h1>';
+document.body.appendChild(header);
+
+const homeSection = document.createElement('section');
+homeSection.classList.add('home');
+homeSection.id = 'home';
+homeSection.innerHTML = 
+document.body.appendChild(homeSection);
